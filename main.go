2092029c49c7bd91a23e3556165592b5d887f51e
Rafał Selewońko <rafal@selewonko.com>
2021-11-04 20:35:07 +0100

    use position service

--- main.go
+++ main.go
@@ -2,48 +2,19 @@ package main
 
 import (
 	"fmt"
-	"image"
-
-	"github.com/kbinani/screenshot"
 )
 
-type Grabber struct {
-	display int
-	bounds  image.Rectangle
-}
-
-func NewGrabber(display int) Grabber {
-	bounds := screenshot.GetDisplayBounds(display)
-	// seems that New World position info is in same position and scale in all resolutions
-	positionInfoBounds := image.Rect(
-		bounds.Max.X-286,
-		bounds.Min.Y+19,
-		bounds.Max.X-5,
-		bounds.Min.Y+35)
-	return Grabber{display, positionInfoBounds}
-}
-
-func (g *Grabber) grab() image.Image {
-	img, err := screenshot.CaptureRect(g.bounds)
-	if err != nil {
-		panic(err)
-	}
-	return img
-}
-
 func main() {
-	display := 0
-
-	grabber := NewGrabber(display)
+	// grabber := NewScreenGrabber(0)
+	grabber := NewFakeGrabber("test.png")
 	ocr := NewOCRClient()
 	ocr.Init()
 	defer ocr.End()
 
-	img := grabber.grab()
-	text := ocr.GetText(img)
-	fmt.Printf("text1 >> %s <<", text)
+	p := NewPositionService(grabber, ocr)
 
-	img = grabber.grab()
-	text = ocr.GetText(img)
-	fmt.Printf("text2 >> %s <<", text)
+	position := p.GetPosition()
+	fmt.Printf("position >> %g <<", position)
+	position = p.GetPosition()
+	fmt.Printf("position >> %g <<", position)
 }
